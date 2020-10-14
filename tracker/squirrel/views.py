6379878f545b12fe11706d7de15256c24266bb7a
SizheSong <ss6159@columbia.edu>
2020-10-14 23:39:00 +0800

    remove useless

--- tracker/squirrel/views.py
+++ tracker/squirrel/views.py
@@ -38,7 +38,6 @@ class IndexView(ListView):
         context = super().get_context_data(**kwargs)
         current_page = context['page_obj'].number
         context['start'] = (current_page - 1) * 10
-        # print('context:',context)
         return context
 
 
@@ -65,12 +64,6 @@ def squirrel(request, unique_squirrel_id):
 
 
 def stats(request):
-    """
-    统计数据
-    1.按天数统计红松鼠和灰松鼠
-    :param request:
-    :return:
-    """
     squirrels = squirrel_operation.get_all_squirrel()
     color_info_list, all_date = squirrel_operation.get_squirrel_color_by_day(squirrels)
     squirrel_operation.get_squirrel_age_by_location(squirrels)
@@ -117,50 +110,3 @@ def squirrel_add(request):
         'status': 'success',
     }
     return JsonResponse(res)
-
-
-def input_data(request):
-    base_path = os.getcwd()
-    path = os.path.join(base_path, 'data/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
-
-    csv_reader = csv.reader(open(path))
-    for index,row in enumerate(csv_reader):
-        if index!=0:
-            sq_info = row
-            create_dict = {
-                "x": sq_info[0],
-                "y": sq_info[1],
-                "unique_squirrel_id": sq_info[2],
-                "hectare": sq_info[3],
-                "shift": sq_info[4],
-                "date": sq_info[5],
-                "hectare_squirrel_number": sq_info[6],
-                "age": sq_info[7],
-                "primary_fur_color": sq_info[8],
-                "highlight_fur_color": sq_info[9],
-                "combination_of_primary_and_highlight_color": sq_info[10],
-                "color_notes": sq_info[11],
-                "location": sq_info[12],
-                "above_ground_sighter_measurement": sq_info[13],
-                "specific_location": sq_info[14],
-                "running": sq_info[15],
-                "chasing": sq_info[16],
-                "climbing": sq_info[17],
-                "eating": sq_info[18],
-                "foraging": sq_info[19],
-                "other_activities": sq_info[20],
-                "kuks": sq_info[21],
-                "quaas": sq_info[22],
-                "moans": sq_info[23],
-                "tail_flags": sq_info[24],
-                "tail_twitches": sq_info[25],
-                "approaches": sq_info[26],
-                "indifferent": sq_info[27],
-                "runs_from": sq_info[28],
-                "other_interactions": sq_info[29],
-                "lat_long": sq_info[30],
-            }
-            s = Squirrel(**create_dict)
-            s.save()
-
-    return HttpResponse("input success")
