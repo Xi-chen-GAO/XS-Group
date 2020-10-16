a04ed48034f502dcd16019ef9f7495136ac6698a
SizheSong <ss6159@columbia.edu>
2020-10-16 23:23:50 +0800

    model change

--- tracker/squirrel/management/commands/import_squirrel_data.py
+++ tracker/squirrel/management/commands/import_squirrel_data.py
@@ -1,6 +1,26 @@
 import csv
+from datetime import datetime
 from django.core.management.base import BaseCommand
 from squirrel.models import Squirrel
+
+
+class Parser:
+    def get_bool(self, data_str):
+        bool_dict = {
+            'true': True,
+            'false': False,
+        }
+        return bool_dict.get(data_str, None)
+
+    def get_date(self, date_str):
+        try:
+            date = datetime.strptime(date_str, "%m%d%Y")
+        except Exception as e:
+            return None
+        return date
+
+
+parser = Parser()
 
 
 class Command(BaseCommand):
@@ -14,7 +34,7 @@ class Command(BaseCommand):
         path = options.get('import_squirrel_data')
         csv_reader = csv.reader(open(path))
         for index, row in enumerate(csv_reader):
-            if index != 0:
+            if index != 0 and index < 10:
                 sq_info = row
                 create_dict = {
                     "x": sq_info[0],
@@ -22,7 +42,7 @@ class Command(BaseCommand):
                     "unique_squirrel_id": sq_info[2],
                     "hectare": sq_info[3],
                     "shift": sq_info[4],
-                    "date": sq_info[5],
+                    "date": parser.get_date(sq_info[5]),
                     "hectare_squirrel_number": sq_info[6],
                     "age": sq_info[7],
                     "primary_fur_color": sq_info[8],
@@ -32,20 +52,20 @@ class Command(BaseCommand):
                     "location": sq_info[12],
                     "above_ground_sighter_measurement": sq_info[13],
                     "specific_location": sq_info[14],
-                    "running": sq_info[15],
-                    "chasing": sq_info[16],
-                    "climbing": sq_info[17],
-                    "eating": sq_info[18],
-                    "foraging": sq_info[19],
+                    "running": parser.get_bool(sq_info[15]),
+                    "chasing": parser.get_bool(sq_info[16]),
+                    "climbing": parser.get_bool(sq_info[17]),
+                    "eating": parser.get_bool(sq_info[18]),
+                    "foraging": parser.get_bool(sq_info[19]),
                     "other_activities": sq_info[20],
-                    "kuks": sq_info[21],
-                    "quaas": sq_info[22],
-                    "moans": sq_info[23],
-                    "tail_flags": sq_info[24],
-                    "tail_twitches": sq_info[25],
-                    "approaches": sq_info[26],
-                    "indifferent": sq_info[27],
-                    "runs_from": sq_info[28],
+                    "kuks": parser.get_bool(sq_info[21]),
+                    "quaas": parser.get_bool(sq_info[22]),
+                    "moans": parser.get_bool(sq_info[23]),
+                    "tail_flags": parser.get_bool(sq_info[24]),
+                    "tail_twitches": parser.get_bool(sq_info[25]),
+                    "approaches": parser.get_bool(sq_info[26]),
+                    "indifferent": parser.get_bool(sq_info[27]),
+                    "runs_from": parser.get_bool(sq_info[28]),
                     "other_interactions": sq_info[29],
                     "lat_long": sq_info[30],
                 }
--- tracker/squirrel/models.py
+++ tracker/squirrel/models.py
@@ -5,34 +5,50 @@ from django.db import models
 
 
 class Squirrel(models.Model):
-    x = models.CharField(max_length=200, null=True)
-    y = models.CharField(max_length=200, null=True)
+    AGE_CHOICES = (
+        ("Adult", "ADULT"),
+        ("Juvenile", "JUVENILE"),
+        ("?", "?"),
+    )
+
+    SHIFT_CHOICES = (
+        ("PM", "PM"),
+        ("AM", "AM"),
+    )
+
+    LOCATION_CHOICES = (
+        ("Ground Plane", "GROUND PLANE"),
+        ("Above Ground", "ABOVE GROUND"),
+    )
+
+    x = models.FloatField(null=True)
+    y = models.FloatField(null=True)
     unique_squirrel_id = models.CharField(max_length=200, null=True)
     hectare = models.CharField(max_length=200, null=True)
-    shift = models.CharField(max_length=200, null=True)
-    date = models.CharField(max_length=200, null=True)
-    hectare_squirrel_number = models.CharField(max_length=200, null=True)
-    age = models.CharField(max_length=200, null=True)
+    shift = models.CharField(max_length=200, choices=SHIFT_CHOICES, null=True)
+    date = models.DateField(null=True)
+    hectare_squirrel_number = models.IntegerField(null=True)
+    age = models.CharField(max_length=200, choices=AGE_CHOICES, null=True)
     primary_fur_color = models.CharField(max_length=200, null=True)
     highlight_fur_color = models.CharField(max_length=200, null=True)
     combination_of_primary_and_highlight_color = models.CharField(max_length=200, null=True)
     color_notes = models.CharField(max_length=200, null=True)
-    location = models.CharField(max_length=200, null=True)
+    location = models.CharField(max_length=200, choices=LOCATION_CHOICES, null=True)
     above_ground_sighter_measurement = models.CharField(max_length=200, null=True)
     specific_location = models.CharField(max_length=200, null=True)
-    running = models.CharField(max_length=200, null=True)
-    chasing = models.CharField(max_length=200, null=True)
-    climbing = models.CharField(max_length=200, null=True)
-    eating = models.CharField(max_length=200, null=True)
-    foraging = models.CharField(max_length=200, null=True)
+    running = models.BooleanField(null=True)
+    chasing = models.BooleanField(null=True)
+    climbing = models.BooleanField(null=True)
+    eating = models.BooleanField(null=True)
+    foraging = models.BooleanField(null=True)
     other_activities = models.CharField(max_length=200, null=True)
-    kuks = models.CharField(max_length=200, null=True)
-    quaas = models.CharField(max_length=200, null=True)
-    moans = models.CharField(max_length=200, null=True)
-    tail_flags = models.CharField(max_length=200, null=True)
-    tail_twitches = models.CharField(max_length=200, null=True)
-    approaches = models.CharField(max_length=200, null=True)
-    indifferent = models.CharField(max_length=200, null=True)
-    runs_from = models.CharField(max_length=200, null=True)
+    kuks = models.BooleanField(null=True)
+    quaas = models.BooleanField(null=True)
+    moans = models.BooleanField(null=True)
+    tail_flags = models.BooleanField(null=True)
+    tail_twitches = models.BooleanField(null=True)
+    approaches = models.BooleanField(null=True)
+    indifferent = models.BooleanField(null=True)
+    runs_from = models.BooleanField(null=True)
     other_interactions = models.CharField(max_length=200, null=True)
     lat_long = models.CharField(max_length=200, null=True)
